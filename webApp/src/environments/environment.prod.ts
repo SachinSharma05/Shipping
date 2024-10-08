f8efa93e863151854f110bd71fe17a0da08b4bc6
SachinSharma05 <sachin.sharma0583@gmail.com>
2024-10-08 14:16:21 +0530

    Removed web app

--- webApp/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://localhost:44384/api'  // Change this to your actual .NET Core API URL
-};
--- webApp/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:44384/api'  // Change this to your actual .NET Core API URL
-};
